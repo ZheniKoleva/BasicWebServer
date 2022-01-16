d0f186a0f7a7549afb81ef688a1c865da53100df
ZheniKoleva <zhenie_koleva@abv.bg>
2022-01-16 10:05:32 +0200

    initial commit

---
+++ BasicWebServer.Server/HTTP/HeaderCollection.cs
@@ -0,0 +1,21 @@
+﻿namespace BasicWebServer.Server.HTTP
+{
+    public class HeaderCollection
+    {
+        private readonly Dictionary<string, Header> headers;
+
+        public HeaderCollection()
+        {
+            headers = new Dictionary<string, Header>();
+        }
+
+        public int Count => headers.Count;
+
+        public void Add(string name, string value)
+        {
+            var header = new Header(name, value);
+
+            headers.Add(name, header);
+        }
+    }
+}
