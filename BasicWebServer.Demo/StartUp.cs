d83fbb0ba7e652fe9eb7c79b4f212365accb0290
ZheniKoleva <zhenie_koleva@abv.bg>
2022-02-07 14:15:31 +0200

    modify StartUp

--- BasicWebServer.Demo/StartUp.cs
+++ BasicWebServer.Demo/StartUp.cs
@@ -2,26 +2,22 @@
 using BasicWebServer.Server.Controllers;
 
 public class StartUp
-{ 
+{
     public static async Task Main()
-    {
-        var server = new HttpServer(routes => routes
-        .MapGet<HomeController>("/", c => c.Index())
-        .MapGet<HomeController>("/Redirect", c => c.Redirect())
-        .MapGet<HomeController>("/HTML", c => c.Html())
-        .MapPost<HomeController>("/HTML", c => c.HtmlFormPost())
-        .MapGet<HomeController>("/Content", c => c.Content())
-        .MapPost<HomeController>("/Content", c => c.DownloadContent())
-        .MapGet<HomeController>("/Cookies", c => c.Cookies())
-        .MapGet<HomeController>("/Session", c => c.Session())
-        .MapGet<UserController>("/Login", c => c.Login())
-        .MapPost<UserController>("/Login", c => c.LogInUser())
-        .MapGet<UserController>("/Logout", c => c.Logout())
-        .MapGet<UserController>("/UserProfile", c => c.GetUserData()));
-
-        await server.Start();
-    }
-   
+        => await new HttpServer(routes => routes
+            .MapGet<HomeController>("/", c => c.Index())
+            .MapGet<HomeController>("/Redirect", c => c.Redirect())
+            .MapGet<HomeController>("/HTML", c => c.Html())
+            .MapPost<HomeController>("/HTML", c => c.HtmlFormPost())
+            .MapGet<HomeController>("/Content", c => c.Content())
+            .MapPost<HomeController>("/Content", c => c.DownloadContent())
+            .MapGet<HomeController>("/Cookies", c => c.Cookies())
+            .MapGet<HomeController>("/Session", c => c.Session())
+            .MapGet<UserController>("/Login", c => c.Login())
+            .MapPost<UserController>("/Login", c => c.LogInUser())
+            .MapGet<UserController>("/Logout", c => c.Logout())
+            .MapGet<UserController>("/UserProfile", c => c.GetUserData()))
+        .Start();
 }
 
 
