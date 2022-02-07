5f86aefa6a7a470f867b519011fb9428d8c73745
ZheniKoleva <zhenie_koleva@abv.bg>
2022-02-07 15:15:59 +0200

    rename UserController class to UsersControleer class

---
+++ BasicWebServer.Demo/Controllers/UsersController.cs
@@ -0,0 +1,64 @@
+﻿using BasicWebServer.Server.Controllers;
+using BasicWebServer.Server.HTTP;
+
+namespace BasicWebServer.Demo.Controllers
+{
+    public class UsersController : Controller
+    {  
+        private const string Username = "user";
+
+        private const string Password = "user123";
+
+
+        public UsersController(Request request)
+            : base(request)
+        {
+        }
+
+        public Response Login() => View();
+
+        public Response LogInUser()
+        {
+            Request.Session.Clear();
+
+            var usernameMatches = Request.Form["Username"] == Username;
+            var passwordMatches = Request.Form["Password"] == Password;
+
+            if (usernameMatches && passwordMatches)
+            {
+                if (!Request.Session.ContainsKey(Session.SessionUserKey))
+                {
+                    Request.Session[Session.SessionUserKey] = "MyUserId";
+
+                    var cookies = new CookieCollection();
+                    cookies.Add(Session.SessionCookieName, Request.Session.Id);
+
+                    return Html("<h3>Logged successfully!</h3>", cookies);
+                }
+
+                return Html("<h3>Logged successfully!</h3>");
+            }
+
+            return Redirect("/Login");
+        }
+
+
+        public Response GetUserData()
+        {
+            if (Request.Session.ContainsKey(Session.SessionUserKey))
+            {
+                return Html($"<h3>Currently logged-in user is with username '{Username}'</h3>");
+            }
+
+            return Redirect("/Login");
+        }
+
+
+        public Response Logout()
+        {
+            Request.Session.Clear();
+
+            return Html("<h3>Loggout successfully!</h3>");
+        }
+    }
+}
