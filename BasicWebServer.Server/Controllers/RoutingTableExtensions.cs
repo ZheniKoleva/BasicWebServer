ac15fa2eced773ae1bf16719ded3d287f1252b46
ZheniKoleva <zhenie_koleva@abv.bg>
2022-02-07 11:51:46 +0200

    add RoutingTableExtension class

---
+++ BasicWebServer.Server/Controllers/RoutingTableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using BasicWebServer.Server.HTTP;
+using BasicWebServer.Server.Routing;
+namespace BasicWebServer.Server.Controllers
+{
+    public static class RoutingTableExtensions
+    {
+        public static IRoutingTable MapGet<TController>(
+            this IRoutingTable routing,
+            string path,
+            Func<TController, Response> controllerFunction)
+            where TController : Controller
+            => RoutingTable.MapPost(path, request => controllerFunction(
+                CreateController<TController>(request)));
+
+        private static TController CreateController<TController>(Request request)
+            => (TController)Activator.CreateInstance(typeof(TController), new[] { request });
+       
+    }
+}
