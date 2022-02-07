73f7dfe2e23d324538d5d5e0f9fc900c8dbdaee0
ZheniKoleva <zhenie_koleva@abv.bg>
2022-02-07 11:52:01 +0200

    create HomeController

---
+++ BasicWebServer.Server/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using BasicWebServer.Server.HTTP;
+
+namespace BasicWebServer.Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(Request request) 
+            : base(request)
+        {
+        }
+
+        public Response Index() => Text("Hello from the server!");
+    }
+}
