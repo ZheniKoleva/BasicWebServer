dad4d2a370842d38e7754a572041a02bfc360af2
ZheniKoleva <zhenie_koleva@abv.bg>
2022-02-07 12:15:47 +0200

    add MapPost method

--- BasicWebServer.Server/Controllers/RoutingTableExtensions.cs
+++ BasicWebServer.Server/Controllers/RoutingTableExtensions.cs
@@ -9,8 +9,17 @@ namespace BasicWebServer.Server.Controllers
             string path,
             Func<TController, Response> controllerFunction)
             where TController : Controller
-            => routingTable.MapPost(path, request => controllerFunction(
+            => routingTable.MapGet(path, request => controllerFunction(
                 CreateController<TController>(request)));
+
+
+        public static IRoutingTable MapPost<TController>(
+           this IRoutingTable routingTable,
+           string path,
+           Func<TController, Response> controllerFunction)
+           where TController : Controller
+           => routingTable.MapPost(path, request => controllerFunction(
+               CreateController<TController>(request)));
 
         private static TController CreateController<TController>(Request request)
             => (TController)Activator.CreateInstance(typeof(TController), new[] { request });
